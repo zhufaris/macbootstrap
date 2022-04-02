ed2caf212bd49ba925bfae1475ac1a8415d13d0e
feiyu <zhufaris@gmail.com>
2022-04-02 13:47:09 +0800

    Update hook_login.sh

--- install-steps/hook_login.sh
+++ install-steps/hook_login.sh
@@ -1,6 +1,3 @@
 if [[ ! -d $HOME/Library/LaunchAgents ]]; then
     mkdir $HOME/Library/LaunchAgents
 fi
-
-sudo cp config/com.bestswifter.onlogin.plist ~/Library/LaunchAgents/
-sudo launchctl load ~/Library/LaunchAgents/com.bestswifter.onlogin.plist
